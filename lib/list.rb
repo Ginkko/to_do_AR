4b2c183dee27ed20413af8555342e9793ac4e291
Alec Arme & Ben Cornelis <student@epicodus.com>
2015-05-05 09:40:13 -0700

    create initial file structure and list class, with specs for save and == methods

---
+++ lib/list.rb
@@ -0,0 +1,32 @@
+class List
+  attr_reader :name, :id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(other_list)
+    same_name = @name == other_list.name
+    same_id = @id == other_list.id
+    same_name && same_id
+  end
+
+  def self.all
+    lists = []
+    results = DB.exec("SELECT * FROM lists;")
+    results.each do |result|
+      name = result.fetch("name")
+      id = result.fetch("id").to_i
+      lists.push(List.new({:name => name, :id => id}))
+    end
+    lists
+  end
+
+  def save
+    result = DB.exec("INSERT INTO lists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
+
+end
---
+++ spec/list_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe(List) do
+
+  describe("#save") do
+    it("add the list to the database") do
+      test_list = List.new({:name => "new list", :id => nil})
+      test_list.save()
+      expect(List.all()).to(eq([test_list]))
+    end
+  end
+
+  describe("#==") do
+    it("is true for lists with the same name and id") do
+      list1 = List.new({:name => "new list", :id => nil})
+      list2 = List.new({:name => "new list", :id => nil})
+      expect(list1).to(eq(list2))
+    end
+  end
+end
